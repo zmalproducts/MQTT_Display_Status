cc25a3f6f96cfa802e632accf12cba48e28db17f
zmalproducts <blog@zmalproducts.net>
2022-01-25 22:51:01 +0100

    testcommit

--- src/main.cpp
+++ src/main.cpp
@@ -99,7 +99,7 @@ void print_wakeup_reason(){
   wakeup_reason = esp_sleep_get_wakeup_cause();
 
   switch(wakeup_reason){
-    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_Io"); break;
     case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
     case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
     case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
